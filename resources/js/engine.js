1b2159b62482061fcf4adb5b3dfb035f109316ef
Marvin Lincoln Kassabian <marvin.kassabian@gmail.com>
2015-04-09 15:40:01 -0400

    Seperated engine module into seperate file

--- resources/js/engine.js
+++ resources/js/engine.js
@@ -1,1 +1,16 @@
-document.writeln('Hello, world!');
+var Engine = (function(Engine) {
+
+  Engine.Player = function(x, y, direction) {
+    this.x = x;
+    this.y = y;
+    this.direction = direction;
+  };
+
+  Engine.Map = function(size) {
+    this.size = size;
+    this.wallGrid = new Uint8Array(size * size);
+  };
+
+  return Engine;
+
+}(Engine || {}));
