71ae6d303085299e9be50ca9bf699f75db9662fc
Marvin Lincoln Kassabian <marvin.kassabian@gmail.com>
2015-04-10 20:35:30 -0400

    Made main.js code not global, changed refrence in index.html

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,8 +1,11 @@
-var Player = Engine.Player;
-var Map = Engine.Map;
+(function() {
 
-var player1 = new Player(0, 2, 3.4);
-var player2 = new Player(1, 43, 65.4);
+  var Player = Engine.Player;
+  var Map = Engine.Map;
 
-console.log(player1);
-console.log(player2);
+  var player1 = new Player(0, 2, 3.4);
+  var player2 = new Player(1, 43, 65.4);
+
+  console.log(player1);
+  console.log(player2);
+})();
