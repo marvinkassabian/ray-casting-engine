343193cdf6e736dbae8541c730e695a06007ce24
Marvin Lincoln Kassabian <marvin.kassabian@gmail.com>
2015-04-11 19:36:00 -0400

    GameLoop code added

--- resources/js/gameloop.js
+++ resources/js/gameloop.js
@@ -10,7 +10,24 @@
     return module;
 
     function GameLoop() {
+      this.frame = this.frame.bind(this);
+      this.lastTime = 0;
+      this.callback = function() {};
     }
+
+    GameLoop.prototype.start = function(callback) {
+      this.callback = callback;
+      window.requestAnimationFrame(this.frame);
+    };
+
+    GameLoop.prototype.frame = function(time) {
+      var seconds = (time - this.lastTime) / 1000; // TODO: remove magic number
+      this.lastTime = time;
+      if (seconds < 0.2) { // TODO: remove magic number
+        this.callback(seconds);
+      }
+      window.requestAnimationFrame(this.frame);
+    };
 
   })(ENGINE.GAMELOOP);
 
