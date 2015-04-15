ecd5584320993338f2bff74babb047e8e5533d24
Marvin Lincoln Kassabian <marvin.kassabian@gmail.com>
2015-04-15 17:15:17 -0400

    Automated minification of .js files

---
+++ Gruntfile.js
@@ -0,0 +1,33 @@
+module.exports = function(grunt) {
+  "use strict";
+
+  // Project configuration.
+  grunt.initConfig({
+    uglify: {
+      my_target : {
+        options : {
+          sourceMap : true,
+          sourceMapName : 'source.map'
+        },
+        files : {
+          'raycaster.min.js' : [
+            'resources/js/engine.js',
+            'resources/js/player.js',
+            'resources/js/controls.js',
+            'resources/js/bitmap.js',
+            'resources/js/map.js',
+            'resources/js/camera.js',
+            'resources/js/gameloop.js',
+            'resources/js/main.js']
+        }
+      }
+    }
+  });
+
+  // Load the plugin that provides the "uglify" task.
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+
+  // Default task(s).
+  grunt.registerTask('default', ['uglify']);
+
+};
